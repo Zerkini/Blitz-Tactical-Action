2cb17516c194c5722f739524dc08d817f7ca7972
jsolawa <jakub.solawa@comarch.pl>
2018-02-08 16:40:37 +0100

    example project

---
+++ Se-shua/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rigidbody2D;
+
+    public float speed;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movementVector = new Vector2(moveHorizontal, moveVertical);
+        rigidbody2D.AddForce(movementVector * speed);
+    }
+}
